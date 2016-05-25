cd40495d08992ea4e8c59453a72892180f666ff3
Leticia Lourenco <leticia@Leticias-iMac.lan>
2016-05-24 17:01:18 -0700

    Google Maps API

--- js/script.js
+++ js/script.js
@@ -7,13 +7,22 @@ function loadData() {
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
 
-    // clear out old data before new request
+    /* clear out old data before new request
+    */
     $wikiElem.text("");
     $nytElem.text("");
-
-    // load streetview
-
-    // YOUR CODE GOES HERE!
+   
+    /* get the values of form inputs
+    */
+    var street = $('#street').val();
+    var city = $('#city').val();
+    /* edit greeting message
+    */
+    $greeting.text('You want to live at '+ street + ', ' + city + '?');
+    /* load Google Streetview
+    */
+    var mapsUrl = 'https://maps.googleapis.com/maps/api/streetview?size=1200x600&location='+ street + '+' + city;
+    $body.append('<img class="bgimg" src="'+ mapsUrl +'">');
 
     return false;
 };
